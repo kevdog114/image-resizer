e46ff59cb078f8e59eaa7bf9bb3c08646c4eaa6e
Schaefer, Kevin <kevin.schaefer@corteva.com>
2021-03-11 19:24:07 -0600

    more work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ interface FileWithPreview {
   preview: any;
   width: any;
   height: any;
+  widthNew: number;
+  heightNew: number;
+  sizeNew: number;
 }
 @Component({
   selector: 'app-root',
@@ -14,10 +17,35 @@ interface FileWithPreview {
 export class AppComponent {
   title = 'angular-image-resizer';
 
+  private LOCALSTORAGE_Keys = {
+    SelectedResizeOption: "selectedResizeOption",
+    CustomSize: "customSize"
+  };
+
+  sizeOptions = [
+    { id: "1", value: "500x500 px", resizeHeight: 500, resizeWidth: 500 },
+    { id: "2", value: "1024x1024 px", resizeHeight: 1024, resizeWidth: 1024 },
+    { id: "3", value: "2048x2048 px", resizeHeight: 2048, resizeWidth: 2048 },
+    { id: "4", value: "Custom Size", resizeHeight: null, resizeWidth: null, isCustom: true }
+  ];
+  selectedSize: string;
   
   @ViewChild("fileDropRef", { static: false }) fileDropEl: ElementRef;
   files: FileWithPreview[] = [];
 
+  constructor() {
+    var previousValue = localStorage.getItem(this.LOCALSTORAGE_Keys.SelectedResizeOption);
+    if(previousValue !== null && this.sizeOptions.filter(a => a.id == previousValue)) {
+      this.selectedSize = previousValue;
+    }
+    var previousCustomSize = localStorage.getItem(this.LOCALSTORAGE_Keys.CustomSize);
+    if(previousCustomSize !== null)
+      this.handleCustomSize(previousCustomSize);
+  }
+
+  clearStorage = () => {
+    localStorage.clear();
+  }
   /**
    * on file drop handler
    */
@@ -31,6 +59,82 @@ export class AppComponent {
    */
   fileBrowseHandler(files) {
     this.prepareFilesList(files);
+  }
+
+  selectOption = (option) => {
+    this.selectedSize = option.id;
+    localStorage.setItem(this.LOCALSTORAGE_Keys.SelectedResizeOption, this.selectedSize);
+  }
+
+  sizeButtonClick = (option) => {
+    if(option.isCustom)
+      this.createCustomSize();
+      
+    this.selectOption(option);
+  }
+
+  createCustomSize = () => {
+    let customSize = prompt(`Please enter a custom image size, in pixels. Please use this exact format: "{width},{height}", or "{side}" if width and height are the same.
+    Example: "250,350" to resize to 250px wide by 350px height.
+    Example: "250" to resize to 250px wide by 250px height.
+    `);
+
+    this.handleCustomSize(customSize);
+  }
+
+  private handleCustomSize = (sz: string) => {
+    if(sz != null) {
+
+      var parsedCustomSize = this.parseCustomSize(sz);
+
+      if(parsedCustomSize === null)
+        return;
+
+      console.log("Custom size parsed", parsedCustomSize);
+      var selectedOption = this.sizeOptions.filter(a => a.isCustom)[0];
+      selectedOption.resizeHeight = parsedCustomSize.height;
+      selectedOption.resizeWidth = parsedCustomSize.width;
+
+      localStorage.setItem(this.LOCALSTORAGE_Keys.CustomSize, sz);
+    }
+  }
+
+  private parseCustomSize = (sz: string) => {
+    
+    var matches = sz.match(/(\d+)(,(\d+))?/);
+    console.log(matches);
+
+    // match[1] contains the width (or side, if the height isn't specified)
+    // match[2] contains the height (if specified)
+    let width = matches[1] === undefined ? 0 : Number(matches[1]);
+    let height = matches[3] === undefined ? undefined : Number(matches[3]);
+    let isBad = false;
+    if(width < 50 || width > 5000) {
+      isBad = true;
+      alert("The width was outside the allowed range of 50px to 5000px");
+    }
+    if(height !== null && (height < 50 || height > 5000)) {
+      isBad = true;
+      alert("The height was outside the allowed range of 50px to 5000px");
+    }
+
+    console.log(height);
+    
+    if(isBad) {
+      this.selectOption(this.sizeOptions[0]);
+      return null;
+    }
+
+    if(height !== undefined)
+      return {
+        width: width,
+        height: height
+      };
+    else
+      return {
+        width: width,
+        height: width
+      };
   }
 
   /**
@@ -53,18 +157,28 @@ export class AppComponent {
           let img = new Image();
           img.src = ev.target.result as string;
 
+
           img.onload = () => {
-            console.log("Adding image: ", {
-              width: img.width,
-              height: img.height,
-              src: img.src
-            });
+
+            var selectedSizeElement = this.sizeOptions.filter(a => a.id == this.selectedSize);
+            var resizeHeight: number = 500;
+            var resizeWidth: number = 500;
+            if(selectedSizeElement != null && selectedSizeElement.length > 0)
+            {
+              resizeHeight = selectedSizeElement[0].resizeHeight;
+              resizeWidth = selectedSizeElement[0].resizeWidth; 
+            }
+            var resizedImg = this.resizeImage(img, resizeWidth, resizeHeight, false, "transparent", file.type);
+
             this.files.push({
               File: file,
               //preview: ev.target.result,
-              preview: this.resizeImage(img, 500, 500, false, "transparent"),
+              preview: resizedImg.image,
               width: img.width,
               height: img.height,
+              widthNew: Math.round(resizedImg.dimensions.w),
+              heightNew: Math.round(resizedImg.dimensions.h),
+              sizeNew: atob(resizedImg.image.replace(/^data:image\/(png|jpeg|jpg);base64,/, '')).length
             });
           }
         };
@@ -82,7 +196,7 @@ export class AppComponent {
     }, 3000);
   }
 
-  resizeImage = ( img: HTMLImageElement, thumbwidth: number, thumbheight: number, crop: boolean, background: string | CanvasGradient | CanvasPattern ) => {
+  resizeImage = ( img: HTMLImageElement, thumbwidth: number, thumbheight: number, crop: boolean, background: string | CanvasGradient | CanvasPattern, imageMimeType: string ) => {
     var c = document.createElement("canvas");
     var cx = c.getContext("2d");
 
@@ -103,7 +217,10 @@ export class AppComponent {
       img, dimensions.x, dimensions.y, dimensions.w, dimensions.h
     );
     
-    return c.toDataURL();
+    return {
+      image: c.toDataURL(imageMimeType),
+      dimensions: dimensions
+    };
   };
 
   resize = ( imagewidth, imageheight, thumbwidth, thumbheight ) => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DndDirective } from './dnd.directive';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { DndDirective } from './dnd.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
